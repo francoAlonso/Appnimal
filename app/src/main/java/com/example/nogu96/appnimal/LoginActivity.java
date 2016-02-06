968c71f6cd0f10ebd2b1c04123fb2956eaec7642
francoAlonso <fra.n.co@hotmail.com>
2016-02-06 01:12:17 -0300

    SignInButton
    Costumizar el boton login para que sea uno de google. Problema porque
    creo que esta en ingles

--- app/src/main/java/com/example/nogu96/appnimal/LoginActivity.java
+++ app/src/main/java/com/example/nogu96/appnimal/LoginActivity.java
@@ -2,16 +2,20 @@ package com.example.nogu96.appnimal;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.w3c.dom.Text;
@@ -19,8 +23,7 @@ import org.w3c.dom.Text;
 
 public class LoginActivity extends Activity {
 
-    Button btnLogin;
-    TextView mStatusTextView;
+    SignInButton btnLogin;
     MensajeToast toast;
     String url = "http://192.168.0.6/ejercicio/Appnimal/index.php";
 
@@ -34,8 +37,8 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = (Button)findViewById(R.id.btnLogin);
-        mStatusTextView = (TextView)findViewById(R.id.signed_in_fmt);
+        btnLogin = (SignInButton) findViewById(R.id.btnLogin);
+        btnLogin.setSize(SignInButton.SIZE_WIDE);
 
         //[Start configure sign_in]
         // Configure sign-in to request the user's ID, email address, and basic
@@ -48,9 +51,11 @@ public class LoginActivity extends Activity {
         // [START build_client]
         // Build a GoogleApiClient with access to the Google Sign-In API and the
         // options specified by gso.
+        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+                .addApi(AppIndex.API).build();
         // [END build_client]
 
         //login
@@ -83,8 +88,48 @@ public class LoginActivity extends Activity {
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
-            mStatusTextView.setText("hola " + acct.getEmail());
+            toast = new MensajeToast(getApplicationContext(), acct.getEmail());
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mGoogleApiClient.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Login Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.nogu96.appnimal/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Login Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.nogu96.appnimal/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
+        mGoogleApiClient.disconnect();
     }
     // [END handleSignInResult]
 
